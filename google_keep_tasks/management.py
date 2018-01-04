f577f579e4b77dc3c02a09372aa37cc8ce5855f2
Nekmo <contacto@nekmo.com>
2018-01-04 01:13:18 +0100

    Auth param.

--- google_keep_tasks/management.py
+++ google_keep_tasks/management.py
@@ -13,7 +13,7 @@ from google_keep_tasks.auth import get_auth
 @click.pass_context
 def cli(ctx, debug, auth):
     keep = gkeepapi.Keep()
-    success = keep.login(*get_auth())
+    success = keep.login(*get_auth(auth))
     ctx.obj = {'keep': keep}
 
 
