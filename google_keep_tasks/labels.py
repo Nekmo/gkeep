8c7340d3fe5dc1185700c4a0562f14c30cd3e5e9
Nekmo <contacto@nekmo.com>
2020-07-21 20:57:41 +0200

    Issue #6: Labels commands (add label)

--- google_keep_tasks/labels.py
+++ google_keep_tasks/labels.py
@@ -1,4 +1,7 @@
+import sys
+
 import click
+from gkeepapi.exception import LabelException
 
 from google_keep_tasks.utils import pretty_date
 
@@ -46,3 +49,25 @@ def list_labels(ctx, timestamps):
     click.echo(u'\n'.join([
          fmt(label) for label in keep.labels()]
     ))
+
+
+@labels.command('add')
+@click.argument('title')
+@click.pass_context
+def add_label(ctx, title):
+    """Create a label on Google Keep. For example:
+
+    .. code-block:: shell
+
+        gkeep labels create "Label name"
+
+    The syntax is:
+    """
+    keep = ctx.obj['keep']
+    try:
+        keep.createLabel(title)
+    except LabelException as e:
+        click.echo(u'Error creating label: {}'.format(e), err=True)
+        sys.exit(3)
+    keep.sync()
+    click.echo(f'Created label {title}')
